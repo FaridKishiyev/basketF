cbc5d9147ac00fcec19b61c65d95b79c7c33f909
FaridKishiyev <tu7nvyvez@code.edu.az>
2022-06-07 13:33:19 +0400

    first commit

---
+++ Flowers/Models/Flower.cs
@@ -0,0 +1,15 @@
+﻿namespace Flowers.Models
+{
+    public class Flower
+    {
+        public int Id { get; set; }
+        public string Flowername { get; set; }
+        public string FlowerImg { get; set; }
+        public float FlowerPricing { get; set; }
+        public string FlowerDescription { get; set; }
+        public int Count { get; set; }
+
+        public int FlowerCategoryId { get; set; }
+        public FlowerCategory Category { get; set; }
+    }
+}
---
+++ Flowers/ViewModels/HomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Flowers.Models;
+using System.Collections.Generic;
+
+namespace Flowers.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Flower> Flowers { get; set; } 
+        public IEnumerable<FlowerCategory> Category { get; set; }
+    }
+}
